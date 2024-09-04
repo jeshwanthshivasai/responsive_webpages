8ebf18d705a19b6b6a3a9d85e5dd1e3cf30681d7
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-09-04 14:34:00 +0530

    new

--- APNA COLLEGE 🏢/JAVASCRIPT/Level6-PracticeQ's.js
+++ APNA COLLEGE 🏢/JAVASCRIPT/Level6-PracticeQ's.js
@@ -1,1 +1,36 @@
 //1
+//returns array elements larger than the number
+
+let array = [8, 9, 10, 1, 2, 3, 4, 5, 6, 7];
+let num = 5;
+function largerElements(arr, num) {
+    for (let i =0; i<num; i++) {
+        if(arr[i] > num) {
+            console.log(arr[i]);
+        }
+    }
+}
+largerElements(array, num);
+
+//2
+//extract unqiue characters from a string
+
+let string = "abcdabcdefgggh";
+function unqchar(string) {
+    let ans = '';
+    for(let i=0; i<string.length; i++) {
+        let char = string[i];
+        if(ans.indexOf(char) == -1) {
+            ans += char;
+        }
+    }
+    return ans;
+}
+unqchar(string);
+
+//3
+//function accepting a list of country names and returns the longest country name as output
+country = ['United States of America', 'Germany', 'Australia'];
+function longestname(country) {
+    
+}
